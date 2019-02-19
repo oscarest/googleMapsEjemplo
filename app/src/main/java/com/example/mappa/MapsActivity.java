724da22ac38404d4037ac5b1acaa48c5e58548a3
rafamolino <Xxracso40xX@gmail.com>
2019-02-19 10:16:50 +0100

    adelanto ejercicio google maps

--- app/src/main/java/com/example/mappa/MapsActivity.java
+++ app/src/main/java/com/example/mappa/MapsActivity.java
@@ -3,6 +3,8 @@ package com.example.mappa;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.mappa.Logic.LogicMapa;
@@ -22,14 +24,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     Mapa m;
-
+    Button boton1;
+    EditText edit1;
+    EditText edit2;
+    EditText edit3;
+    EditText edit4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        boton1 = findViewById(R.id.button);
+        edit1 = findViewById(R.id.editText);
+        edit2 = findViewById(R.id.editText2);
+        edit3 = findViewById(R.id.editText3);
+        edit4 = findViewById(R.id.editText4);
+        boton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+    public void insertarProducto(View view) {
+        String descripcion = edit1.getText().toString();
+        String longitud = edit2.getText().toString();
+        String categoria = edit3.getText().toString();
+        String latitud = edit4.getText().toString();
+            Producto p = new Producto(nombre, Integer.parseInt(cantidad), seccion);
+            LogicProducto.insertarProducto(this, p);
+            mostrarMensaje("El producto " + nombre + " ha sido insertado.");
+    }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -41,12 +68,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //mMap.addMarker(new MarkerOptions().position(Sevilla).title("Ciudad de Sevilla").icon(BitmapDescriptorFactory.fromResource(R.drawable.mi_marcador)));
         //mMap.addMarker(new MarkerOptions().position(Sevilla).title("Ciudad de Sevilla").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE)));
         //mMap.addMarker(new MarkerOptions().position(Huelva).title("Ciudad de Huelva"));
-        List lst = LogicMapa.listaMapa(this);
-
-        for(m : lst)
-        {
-            mMap.addMarker(new MarkerOptions().position(datos).title(m.getDescripcion()));
-        }
+        //List lst = LogicMapa.listaMapa(this);
+        mMap.addMarker(new MarkerOptions().position(datos).title(m.getDescripcion()));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(Sevilla));
 
         mMap.animateCamera(CameraUpdateFactory.zoomIn());
